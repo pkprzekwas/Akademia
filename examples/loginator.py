a7f6e12c1a3ddccd9f759b3a1beb86cc0ec7588e
Patryk Przekwas <pkprzekwas@gmail.com>
2016-08-20 09:22:57 +0200

    Button method refactor
    d
        def fill_in(field, text):
            field.clear()
            field.send_keys(text)

--- examples/loginator.py
+++ examples/loginator.py
@@ -5,24 +5,24 @@ class BasePage(object):
     def __init__(self, driver):
         self._driver = driver
 
-    @staticmethod
-    def fill_in(field, text):
-        field.clear()
-        field.send_keys(text)
-
-    def click_submit_btn(self):
-        button = self.find_element_by(LOCATORS.get("submit_button"))
+    def click_btn(self, locator_name):
+        button = self.find_element_by(LOCATORS.get(locator_name))
         button.click()
 
     def find_element_by(self, locator):
         element = self._driver.find_element(*locator)
         return element
 
+    @staticmethod
+    def fill_in(field, text):
+        field.clear()
+        field.send_keys(text)
+
 
 class Loginator(BasePage):
     def __init__(self, driver):
-        BasePage.__init__(self, driver)
         # super(self, Loginator).__init__(driver)
+        BasePage.__init__(self, driver)
         self._locators = LOCATORS
 
     def login(self, username, password):
@@ -32,4 +32,4 @@ class Loginator(BasePage):
         Loginator.fill_in(username_loc, username)
         Loginator.fill_in(password_loc, password)
 
-        self.click_submit_btn()
+        self.click_btn("submit_button")
--- examples/test_login.py
+++ examples/test_login.py
@@ -8,10 +8,8 @@ class TestLogin(unittest.TestCase):
     def setUp(self):
         config = get_config()
         browser_name = config.get('browser_name')
-
         browser_driver = get_browser(browser_name)
         browser_driver.get('http://localhost:8000/shop/login')
-
         self.loginator = Loginator(browser_driver)
 
     def test_login(self):
